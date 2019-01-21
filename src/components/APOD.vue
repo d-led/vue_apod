66b1994854c24e689213571868f9263309c674d4
Dmitry Ledentsov <dmlled@yahoo.com>
2019-01-21 21:40:31 +0100

    apod demo key ok

---
+++ src/components/APOD.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="apod">
+    <button @click="loadApod">Go!</button>
+    <h1>{{apod.title}}</h1>
+    <img v-if="apod.url" v-bind:src="apod.url">
+    <p>{{apod.explanation}}</p>  
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component({
+  props: {
+      apiKey: String // todo
+  }
+})
+export default class HelloWorld extends Vue {
+  apod = {
+    title: '...',
+    url: '',
+    explanation: '...'
+  }
+  
+  loadApod() {
+    var vm = this
+    fetch("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY")
+    .then(function (response) {
+    return response.json()
+    })
+    .then(function (data) {
+      vm.apod = data
+    })
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
